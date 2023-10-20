748eb06e3eb00aad66750a271bae510ae5486c6d
JimmyCauja <jimmycauja@gmail.com>
2023-10-20 17:19:33 -0500

    Cambios finales en la cromática y tamaño para que tenga un mejor contraste

--- sketch.js
+++ sketch.js
@@ -5,34 +5,15 @@ let radio;
 let fondo;
 let fbolita;
 
-
-
-
 function setup() {
 createCanvas(windowWidth, windowHeight);
 posX = windowWidth / 2;
 posY = windowHeight / 2;
 diametro = 100;
 radio = diametro / 2;
-fondo = color(25, 100, 50);
-fbolita = color(random(0, 255), random(0, 255), random(0, 255));
-// frameRate(10);
-// print(posX);
-// posX = 234;
-// print(posX);
-// posX = posX + 25;
-// print(posX);
-// posX = posX * 2;
-// print(posX);
-// posX = posX / 5;
-// print(posX);
-// posX *= 8;
-// print(posX);
-// posX += 1;
-// print(posX);
-// posX ++;
+fondo = color(15, 60, 50);
+fbolita = color(random(0, 100), random(0, 255), random(0, 150));
 }
-
 
 function draw() {
 background(fondo);
@@ -40,13 +21,12 @@ posX -= 10;
 
 
 if(posX < -radio) {
-diametro = random(10, 100);
+diametro = random(10, 200);
 radio = diametro / 2;
 fondo = color(255, random(100, 255),50);
-fbolita = color(255, random(100, 255),25);
+fbolita = color(240, random(80, 255),25);
 posX = windowWidth + radio;
 }
-
 
 fill(fbolita);
 circle(posX, posY, diametro);
